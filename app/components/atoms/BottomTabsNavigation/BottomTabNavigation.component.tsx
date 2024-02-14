06ce290c440243973074fd9bf905b34a339a691f
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-14 08:59:20 +0200

    created Blue print of BottomTabsNAvigation.
    created BottomTabsNAvigation and some dummy components to test the navigation behavior

---
+++ app/components/atoms/BottomTabsNavigation/BottomTabNavigation.component.tsx
@@ -0,0 +1,46 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text } from 'react-native';
+
+function BottomTabsNavigation() {
+
+    const Tab = createBottomTabNavigator();
+
+    return(
+        <Tab.Navigator>
+            <Tab.Screen name='Screen1' component={Screen1} />
+            <Tab.Screen name='Screen2' component={Screen2} />
+            <Tab.Screen name='Screen3' component={Screen3} />
+            <Tab.Screen name='Screen2' component={Screen2} />
+            <Tab.Screen name='Screen2' component={Screen2} />
+        </Tab.Navigator>
+    )
+    
+}
+
+export default BottomTabsNavigation;
+
+function Screen1(){
+    return(
+        <Text>screen 1</Text>
+    )
+}
+function Screen2(){
+    return(
+        <Text>screen 2</Text>
+    )
+}
+function Screen3(){
+    return(
+        <Text>screen 3</Text>
+    )
+}
+function Screen4(){
+    return(
+        <Text>screen 4</Text>
+    )
+}
+function Screen5(){
+    return(
+        <Text>screen 5</Text>
+    )
+}

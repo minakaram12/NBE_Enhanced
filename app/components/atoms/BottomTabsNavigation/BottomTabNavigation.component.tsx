fc1712cdeab097af28b3b471f6e81c5c15bb9e03
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-14 09:09:25 +0200

    fix typos in BottomNavigation
    fix some typos related to screens

--- app/components/atoms/BottomTabsNavigation/BottomTabNavigation.component.tsx
+++ app/components/atoms/BottomTabsNavigation/BottomTabNavigation.component.tsx
@@ -10,8 +10,8 @@ function BottomTabsNavigation() {
             <Tab.Screen name='Screen1' component={Screen1} />
             <Tab.Screen name='Screen2' component={Screen2} />
             <Tab.Screen name='Screen3' component={Screen3} />
-            <Tab.Screen name='Screen2' component={Screen2} />
-            <Tab.Screen name='Screen2' component={Screen2} />
+            <Tab.Screen name='Screen4' component={Screen4} />
+            <Tab.Screen name='Screen5' component={Screen5} />
         </Tab.Navigator>
     )
     

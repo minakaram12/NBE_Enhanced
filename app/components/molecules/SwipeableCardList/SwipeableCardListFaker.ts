1be0147e6fac8cd5d1e0204cabb94e8d1a9de006
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-17 13:53:26 +0200

    create a faker for swipe list view

---
+++ app/components/molecules/SwipeableCardList/SwipeableCardListFaker.ts
@@ -0,0 +1,76 @@
+import {DetailedCardProps} from '../../atoms/DetailedCard/DetailedCard.component';
+
+export const detailedCardPropsArray: DetailedCardProps[] = [
+  {
+    name: 'John Doe',
+    mobileNumber: '1234567890',
+    balance: '100.00',
+    image: 'url1',
+    color: 'blue',
+  },
+  {
+    name: 'Jane Smith',
+    mobileNumber: '9876543210',
+    balance: '50.00',
+    image: 'url2',
+    // No color and viewStyle specified for this item
+  },
+  {
+    name: 'Alice Johnson',
+    mobileNumber: '5555555555',
+    balance: '75.50',
+    image: 'url3',
+    color: 'green',
+  },
+  {
+    name: 'Bob Williams',
+    mobileNumber: '9998887777',
+    balance: '120.75',
+    image: 'url4',
+    color: 'red',
+  },
+  {
+    name: 'Eva Davis',
+    mobileNumber: '3333333333',
+    balance: '90.25',
+    image: 'url5',
+    // No color and viewStyle specified for this item
+  },
+  {
+    name: 'Michael Johnson',
+    mobileNumber: '4444444444',
+    balance: '60.80',
+    image: 'url6',
+    color: 'purple',
+  },
+  {
+    name: 'Sophia Wilson',
+    mobileNumber: '7777777777',
+    balance: '150.50',
+    image: 'url7',
+    color: 'orange',
+  },
+  {
+    name: 'David Brown',
+    mobileNumber: '6666666666',
+    balance: '80.20',
+    image: 'url8',
+    // No color and viewStyle specified for this item
+  },
+  {
+    name: 'Olivia Davis',
+    mobileNumber: '2222222222',
+    balance: '95.75',
+    image: 'url9',
+    color: 'pink',
+  },
+  {
+    name: 'William White',
+    mobileNumber: '1111111111',
+    balance: '110.00',
+    image: 'url10',
+    color: 'yellow',
+  },
+];
+
+// Now detailedCardPropsArray is an array of 10 items with the DetailedCardProps type

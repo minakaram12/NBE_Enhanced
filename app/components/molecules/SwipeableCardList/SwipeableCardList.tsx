0242df4cc356b480a724b0150d3a22eeb6cd2cf9
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-17 15:18:05 +0200

    Update SwipeableCardList.tsx

--- app/components/molecules/SwipeableCardList/SwipeableCardList.tsx
+++ app/components/molecules/SwipeableCardList/SwipeableCardList.tsx
@@ -1,12 +1,35 @@
-import { useState } from "react";
-import { SwipeListView } from "react-native-swipe-list-view"
-import { DetailedCardProps } from "../../atoms/DetailedCard/DetailedCard.component";
+import {useState} from 'react';
+import {SwipeListView} from 'react-native-swipe-list-view';
+import DetailedCard, {
+  DetailedCardProps,
+} from '../../atoms/DetailedCard/DetailedCard.component';
+import {detailedCardPropsArray} from './SwipeableCardListFaker';
+import {Pressable} from 'react-native';
 
+//i am using a faker if there is no input
 interface SwipeableCardListProps {
-    data:Array<Object>
+  data?: Array<DetailedCardProps>;
 }
 
-const SwipeableCardList: React.FC<SwipeableCardListProps> = ({data}) => {
-    const [listData,setListData] = useState([]);
-  return (<SwipeListView data={} renderItem={} renderHiddenItem={} />);
+const SwipeableCardList: React.FC<SwipeableCardListProps> = () => {
+  const [listData, setListData] = useState(detailedCardPropsArray);
+
+  const renderItem = (data, rowMap)=>{
+    return (
+      <Pressable>
+        <DetailedCard
+          name={data.item.name}
+          mobileNumber={data.item.mobileNumber}
+          balance={data.item.balance}
+          image={data.item.image}
+          color={data.item.color}
+          viewStyle={data.item.ViewStyle}
+        />
+      </Pressable>
+    )}
+
+
+
+  return <SwipeListView data={listData} renderItem={renderItem} />;
 };
+export default SwipeableCardList;

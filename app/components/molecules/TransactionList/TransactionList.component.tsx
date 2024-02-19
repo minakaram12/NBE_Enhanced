1b1573f0248bb584867f8d2e37058a3453bb9a9f
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-19 16:15:40 +0200

    transaction list

---
+++ app/components/molecules/TransactionList/TransactionList.component.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import TransactionCard from '../../atoms/Transactioncard/TransactionCard.component';
+import {TransactionCardProp} from '../../atoms/Transactioncard/TransactionCard.component';
+import {FlatList, StyleSheet, View} from 'react-native';
+interface TransactionListProps {
+  transactions: Array<TransactionCardProp>;
+}
+function TransactionList({transactions}: TransactionListProps) {
+  const renderItem = ({item}: {item: TransactionCardProp}) => {
+    return (
+      <TransactionCard
+        transactionItem={item.transactionItem}
+        date={item.date}
+        transactionValue={item.transactionValue}
+      />
+    );
+  };
+  const itemSepratorComponent = () => {
+    return <View style={styles.sepratorStyle} />;
+  };
+  return (
+    <FlatList
+      data={transactions}
+      renderItem={renderItem}
+      ItemSeparatorComponent={itemSepratorComponent}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  sepratorStyle: {height: 1, backgroundColor: '#B7B7B7'},
+});
+
+export default TransactionList;

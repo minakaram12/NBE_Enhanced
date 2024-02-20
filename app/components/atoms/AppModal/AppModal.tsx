ef4f8218fa57d1fb8ebc542ff5ca6e74c6f447aa
Abdelrahman Yasser <abdelrahman.yasser.365@gmail.com>
2024-02-20 14:00:30 +0200

    Modal

---
+++ app/components/atoms/AppModal/AppModal.tsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import {
+  Alert,
+  DimensionValue,
+  Image,
+  Modal,
+  Pressable,
+  Text,
+  View,
+} from 'react-native';
+
+import styles from './AppModal.style';
+import {layouts} from '../../../constants/styles';
+import MainBtn from '../MainBtn/MainBtn';
+import {px} from '../../../constants/styles/layouts';
+
+interface AppModalProps {
+  modalVisible: boolean;
+  setModalVisible: (value: boolean) => void;
+  imageSource?: any;
+  imageWidth?: DimensionValue;
+  imageHeight?: DimensionValue;
+  titleText?: string;
+  errorTitle?: boolean;
+  descriptionText?: string;
+  mony?: string;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+  onConfirmPress?(): void;
+  onCancelPress?(): void;
+}
+
+const AppModal: React.FC<AppModalProps> = ({
+  modalVisible,
+  setModalVisible,
+  imageSource,
+  imageWidth = 150,
+  imageHeight = 150,
+  titleText,
+  errorTitle = false,
+  descriptionText,
+  confirmButtonText,
+  cancelButtonText,
+  mony,
+  onConfirmPress,
+  onCancelPress,
+}) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+        setModalVisible(!modalVisible);
+      }}>
+      <View style={[layouts.flexed, layouts.allCentered]}>
+        <Pressable
+          onPress={() => setModalVisible(!modalVisible)}
+          style={[
+            layouts.allCentered,
+            layouts.px.xl,
+            layouts.absolute,
+            styles.overlay,
+          ]}
+        />
+        <View
+          style={[
+            layouts.allCentered,
+            layouts.px.xl,
+            layouts.py.xl,
+            layouts.absolute,
+            styles.modalContent,
+          ]}>
+          {imageSource && (
+            <Image
+              source={imageSource}
+              style={{width: px(imageWidth), height: px(imageHeight)}}
+              resizeMode="cover"
+            />
+          )}
+          {titleText && (
+            <Text
+              style={[
+                styles.blackText,
+                styles.modalTitle,
+                layouts.my.lg,
+                errorTitle ? styles.errorText : null,
+              ]}>
+              {titleText}
+            </Text>
+          )}
+          {descriptionText && (
+            <Text style={[styles.modalDescription, layouts.mb.lg]}>
+              {descriptionText}
+            </Text>
+          )}
+          {mony && <Text style={[styles.modalMony]}>{mony}</Text>}
+          <View style={[layouts.row]}>
+            {cancelButtonText && (
+              <MainBtn
+                buttonText={cancelButtonText}
+                variant="outline"
+                onPress={() => {
+                  if (onCancelPress) {
+                    onCancelPress();
+                  }
+                }}
+                buttonStyle={[
+                  layouts.mt.lg,
+                  layouts.me.lg,
+                  {borderColor: 'red'},
+                ]}
+                textStyle={[styles.boldFont, {color: 'red'}]}
+              />
+            )}
+            {confirmButtonText && (
+              <MainBtn
+                buttonText={confirmButtonText}
+                onPress={() => {
+                  if (onConfirmPress) {
+                    onConfirmPress();
+                  }
+                }}
+                buttonStyle={[layouts.mt.lg, layouts.flexed]}
+                textStyle={[styles.boldFont]}
+              />
+            )}
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default AppModal;
--- app/components/screens/SplashScreen/SplashScreen.tsx
+++ app/components/screens/SplashScreen/SplashScreen.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import Logo from '../../../assets/svgs/Logo';
-import {View} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {View, Animated, Easing} from 'react-native';
 import {layouts} from '../../../constants/styles';
-
 import styles from './SplashScreen.style';
+import Logo from '../../../assets/svgs/Logo';
 import NamedLogo from '../../../assets/svgs/NamedLogo';
 
 const SplashScreen = () => {
+  const bounceValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(bounceValue, {
+      toValue: 1.3,
+      duration: 1000,
+      easing: Easing.inOut(Easing.ease),
+      useNativeDriver: true,
+    }).start();
+  }, [bounceValue]);
+
   return (
     <View style={[layouts.flexed, layouts.allCentered, styles.container]}>
       <View style={[layouts.flexed, layouts.allCentered]}>
-        <Logo width={119} height={136} />
+        <Animated.View style={{transform: [{scale: bounceValue}]}}>
+          <Logo width={119} height={136} />
+        </Animated.View>
       </View>
       <NamedLogo width={130} height={40} style={[layouts.my.xxl]} />
     </View>

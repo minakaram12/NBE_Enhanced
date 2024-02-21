15acfcb937dff8ccd091536c6257d9bc518b86cb
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-21 20:03:06 +0200

    Create BeneficiaryListScreen.tsx

---
+++ app/components/screens/BeneficiaryListScreen/BeneficiaryListScreen.tsx
@@ -0,0 +1,11 @@
+import TopNavigator from "../../molecules/TopNavigator/TopNavigator";
+import SimpleOrDetailedList from "../../molecules/simpleOrDetailedList/simpleOrDetailedList";
+
+function BenefiiciaryListScreen() {
+    return(
+        <TopNavigator />
+        <SimpleOrDetailedList />
+    )
+}
+
+export default BenefiiciaryListScreen;

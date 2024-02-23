e060506d4945c9196a83e327a2786cef9b37ba59
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-23 14:39:44 +0200

    Update SimpleCardList.tsx

--- app/components/molecules/SimpleCardList/SimpleCardList.tsx
+++ app/components/molecules/SimpleCardList/SimpleCardList.tsx
@@ -5,25 +5,23 @@ import SimpleCard, {
 import { FlatList } from 'react-native';
 import { layouts } from '../../../constants/styles';
 import NoBeneficiary from '../../atoms/NoBeneficiaries/NoBeneficiaries';
+import { ExtendedCardProps } from '../SwipeableCardList/SwipeableCardListFaker';
 
 interface SimpleCardListProp {
-  cards: Array<SimpleCardProp>;
+  cards: Array<ExtendedCardProps>;
 }
 
-const SimpleCardList: React.FC<SimpleCardListProp> = ({ cards }) => {
-  const keyExtractor = (item: SimpleCardProp) => item.image + item.name;
-  const renderItem = ({ item }: { item: SimpleCardProp }) => {
+const SimpleCardList: React.FC<SimpleCardListProp> = ({cards}) => {
+  const renderItem = ({item}: {item: ExtendedCardProps}) => {
     return <SimpleCard image={item.image} name={item.name} />;
   };
-
   const isCardsExists = cards.length > 0;
-
   return (
     <>
       {isCardsExists ? (
         <FlatList
           data={cards}
-          keyExtractor={keyExtractor}
+
           renderItem={renderItem}
           numColumns={4}
           style={[layouts.px.sm, { backgroundColor: '#F1F3FB' }]}

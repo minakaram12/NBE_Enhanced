37b60cda0977a53eae46174926f724e1af920e37
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-29 02:16:31 +0200

    Update simpleOrDetailedList.tsx

--- app/components/molecules/simpleOrDetailedList/simpleOrDetailedList.tsx
+++ app/components/molecules/simpleOrDetailedList/simpleOrDetailedList.tsx
@@ -5,16 +5,15 @@ import SwipeableCardList from '../SwipeableCardList/SwipeableCardList';
 import BeneficiariesController from '../Beneficiaries-controller/BeneficiariesController.component';
 import {StyleSheet, View} from 'react-native';
 import {layouts} from '../../../constants/styles';
-import {
-  ExtendedCardProps,
-  detailedCardPropsArray,
-} from '../SwipeableCardList/SwipeableCardListFaker';
+import {ExtendedCardProps} from '../SwipeableCardList/SwipeableCardListFaker';
 import NoBeneficiary from '../../atoms/NoBeneficiaries/NoBeneficiaries';
-import { theme } from '../../../theme/theme';
+import {theme} from '../../../theme/theme';
 
 function SimpleOrDetailedList({newCards}) {
   const [colView, setterColView] = useState(true);
-  const [beneficiaries, setBeneficiaries] = useState<Array<ExtendedCardProps>>([]);
+  const [beneficiaries, setBeneficiaries] = useState<Array<ExtendedCardProps>>(
+    [],
+  );
 
   useEffect(() => {
     setBeneficiaries(newCards);
@@ -27,8 +26,7 @@ function SimpleOrDetailedList({newCards}) {
   const isempty = beneficiaries?.length <= 0 || beneficiaries?.length === 0;
 
   return (
-    <View
-      style={[layouts.px.lg, {backgroundColor: theme?.BackgroundScreen}, layouts.fullHeight]}>
+    <View style={[layouts.px.lg, styles.outerVeiw, layouts.fullHeight]}>
       <BeneficiariesController
         style={styles.controller}
         changeViewSetter={setterColView}
@@ -49,5 +47,8 @@ function SimpleOrDetailedList({newCards}) {
     </View>
   );
 }
-const styles = StyleSheet.create({controller: {marginBottom: 10}});
+const styles = StyleSheet.create({
+  controller: {marginBottom: 10},
+  outerVeiw: {backgroundColor: theme?.BackgroundScreen},
+});
 export default SimpleOrDetailedList;

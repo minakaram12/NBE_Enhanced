db241a895c0a6646ad5c6f0eeff01795e49f7c13
Mohamed Mamdouh <mamdouhibr67@gmail.com>
2024-02-29 14:20:44 +0200

    Update HiddenItemWithActions.tsx

--- app/components/atoms/hiddenItemWithActions/HiddenItemWithActions.tsx
+++ app/components/atoms/hiddenItemWithActions/HiddenItemWithActions.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {View, Text, StyleSheet, Pressable} from 'react-native';
 import {layouts} from '../../../constants/styles';
-import { useTheme } from '../../../ContextAPI/ThemeContext';
+import {theme} from '../../../theme/theme';
 
 const HiddenItemWithActions = ({onDelete, onEdit}) => {
-  
   return (
     <View
       style={[
@@ -17,45 +16,27 @@ const HiddenItemWithActions = ({onDelete, onEdit}) => {
       <Pressable onPress={onEdit}>
         <View
           style={[
-            useTheme().isDarkMode.name === 'Dark'
-              ? {backgroundColor: '#007236'}
-              : {backgroundColor: '#007236'},
+            styles.leftBtn,
             layouts.fullHeight,
             layouts.xCentered,
             styles.buttonsWidth,
             styles.curved,
             layouts.yCentered,
           ]}>
-          <Text
-            style={
-              useTheme().isDarkMode.name === 'Dark'
-                ? {color: '#ffffff'}
-                : {backgroundColor: '#000000'}
-            }>
-            Edit
-          </Text>
+          <Text style={styles.textStyle}>Edit</Text>
         </View>
       </Pressable>
       <Pressable onPress={onDelete}>
         <View
           style={[
-            useTheme().isDarkMode.name === 'Dark'
-              ? {backgroundColor: '#7a1712'}
-              : {backgroundColor: '#7a1712'},
+            styles.righBtn,
             layouts.fullHeight,
             layouts.xCentered,
             styles.buttonsWidth,
             styles.curved,
             layouts.yCentered,
           ]}>
-          <Text
-            style={
-              useTheme().isDarkMode.name === 'Dark'
-                ? {color: '#ffffff'}
-                : {backgroundColor: '#000000'}
-            }>
-            Delete
-          </Text>
+          <Text style={styles.textStyle}>Delete</Text>
         </View>
       </Pressable>
     </View>
@@ -66,4 +47,7 @@ const styles = StyleSheet.create({
   container: {height: 89, borderRadius: 18, overflow: 'hidden'},
   buttonsWidth: {width: 100},
   curved: {borderRadius: 18},
+  leftBtn: {backgroundColor: '#007236'},
+  righBtn: {backgroundColor: '#7a1712'},
+  textStyle: {color: theme.textColor},
 });
